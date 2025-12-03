ce3927f06b487ed99fc101ecc2616174dc550bad
Tan560 <97492534+Tan560@users.noreply.github.com>
2025-12-02 23:39:52 -0500

    Merged fixes from year 1

--- src/main/cpp/chassis/commands/TrajectoryDrive.cpp
+++ src/main/cpp/chassis/commands/TrajectoryDrive.cpp
@@ -32,7 +32,7 @@
 TrajectoryDrive::TrajectoryDrive(
     subsystems::CommandSwerveDrivetrain *chassis) : m_chassis(chassis),
                                                     m_pathName(""),
-                                                    //m_trajectoryStates(),
+                                                    // m_trajectoryStates(),
                                                     m_prevPose(),
                                                     m_wasMoving(false),
                                                     m_timer(std::make_unique<frc::Timer>()),
@@ -48,7 +48,14 @@ TrajectoryDrive::TrajectoryDrive(
 void TrajectoryDrive::Initialize()
 {
     m_trajectory = AutonUtils::GetTrajectoryFromPathFile(m_pathName);
-    m_trajectoryStates = m_trajectory.value().samples;
+    if (m_trajectory.has_value())
+    {
+        m_trajectoryStates = m_trajectory.value().samples;
+    }
+    else
+    {
+        m_totalTrajectoryTime = 0_s;
+    }
 
     // Reset and start the timer when the command begins
     m_timer.get()->Reset();
